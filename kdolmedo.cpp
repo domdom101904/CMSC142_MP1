7f7c6696d7075c77577660e44f7ea97c732a6f89
domdom101904 <159731847+domdom101904@users.noreply.github.com>
2025-08-19 05:47:45 +0800

    Update kdolmedo.cpp

--- kdolmedo.cpp
+++ kdolmedo.cpp
@@ -2,3 +2,28 @@
 #include <fstream>
 #include <sstream>
 
+using namespace std;
+
+void readInclude(string);
+
+int main(){
+    string fileName;
+
+    cin>>fileName;
+
+    readInclude(fileName);
+
+}
+
+void readInclude(string file){
+    ifstream reader(file);
+
+    if(reader.is_open()){
+        string line,str;
+        getline(reader, line);
+
+    }
+
+    reader.close();
+
+}
