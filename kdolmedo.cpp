c23b555797ae53630d44593a8ed544d23b5da936
domdom101904 <159731847+domdom101904@users.noreply.github.com>
2025-08-19 06:28:42 +0800

    first phase

--- kdolmedo.cpp
+++ kdolmedo.cpp
@@ -1,3 +1,8 @@
+//Te goal is to simply literally insert the contents of the included files
+//in the c/c++ source file, replacing the preprocessor directives in the 
+//correct order. When a header file is included more than once, insert the 
+//first include and disregard the succeeding ones.
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -21,17 +26,26 @@ void readInclude(string file){
     if(reader.is_open()){
         string line,str;
         //#include "sample.h"
+
         getline(reader, line);
+            //sir wants us to read multiple files
+            //this line returns 0 when it reaches the end of the file
 
         stringstream ss(line);
 
         ss>>str;
 
-        cout<<"first: "<<str<<endl;
+        //cout<<"first: "<<str<<endl;
 
-        ss>>str;
+        if(str.compare("#include")==0){
+            ss>>str;
+            cout<<"included file: "<<str<<endl;
+            string newFile = str.substr(1,str.length()-2);
+            cout<<"new file: "<<newFile<<endl;
+            //readInclude();
+        }
 
-        cout<<"second: "<<str<<endl;
+        //cout<<"second: "<<str<<endl;
 
     }
 
